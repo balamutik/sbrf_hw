98678cf6f7d7a330d54daa7eaf119deb434e4c54
Evgeniy Zvonkov <ronin1984@gmail.com>
2020-04-06 20:34:52 +0300

    init

---
+++ DIYArray/src/main/java/org/sbrf/zvonkov/App.java
@@ -0,0 +1,13 @@
+package org.sbrf.zvonkov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
